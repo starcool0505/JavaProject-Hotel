213814d512fe521e2eecfe158e9b28cdf94e950e
april <aprilshen0401@gmail>
2024-01-29 20:22:00 +0800

    0129 20:21

--- Hotel/src/main/java/com/example/dao/BookDaoImpl.java
+++ Hotel/src/main/java/com/example/dao/BookDaoImpl.java
@@ -9,6 +9,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.entity.Book;
 import com.example.entity.Room;
